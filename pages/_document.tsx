a0681143b8be5a73932c1aa935c605f31c8674fd
박지수 <jisupark2000@gmail.com>
2022-12-22 04:27:28 +0900

    Add: meta-tag content 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,14 @@ export default function Document() {
   return (
     <Html lang='ko'>
       <Head />
+      <link rel='icon' href='/favicon.ico' />
       <link rel='preconnect' href='https://fonts.googleapis.com' />
       <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
       <link
         href='https://fonts.googleapis.com/css2?family=Dancing+Script&family=Nanum+Pen+Script&family=Noto+Sans+KR:wght@300;400;500;700;900&display=swap'
         rel='stylesheet'
       />
+
       <body>
         <Main />
         <NextScript />
